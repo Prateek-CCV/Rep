35ed67dc49a3628bda428b49c0e6e712792b797e
Prateek-CCV <60535464+Prateek-CCV@users.noreply.github.com>
2022-01-12 16:34:23 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,13 +1,10 @@
-<?php
-$hostname="aacbm1xha31js0.c2u0hj6ejqek.us-east-1.rds.amazonaws.com"; //local server name default localhost
-$username="admin";  //mysql username default is root.
-$password="Ideapad330";       //blank if no password is set for mysql.
-$database="aacbm1xha31js0";  //database name which you created
-$con=mysql_connect($hostname,$username,$password);
-if(! $con)
-{
-die('Connection Failed'.mysql_error());
-}
+<?php 
 
-mysql_select_db($database,$con);
+// modify the database properties 
+
+$host = "aacbm1xha31js0.c2u0hj6ejqek.us-east-1.rds.amazonaws.com";
+$user = "admin";
+$pwd = "Ideapad330";
+$db = "aacbm1xha31js0";
+
 ?>
